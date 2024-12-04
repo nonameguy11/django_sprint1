1fd18c652a098a63f516b2cf41e46a728ea7472a
nonameguy11 <110131075+nonameguy11@users.noreply.github.com>
2024-12-05 02:37:19 +1000

    update

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('blog.urls', namespace='blog')),
+#     path('pages/', include('pages.urls', namespace='pages')),
+# ]
+
+urlpatterns = [
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
+    path('admin/', admin.site.urls),
+]
+
+
